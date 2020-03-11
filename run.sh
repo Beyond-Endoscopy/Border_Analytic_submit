2d1b41225f1299d6ef7b006f7310b563e3161e21
Beyond-Endoscopy <57195937+Beyond-Endoscopy@users.noreply.github.com>
2020-03-11 18:36:38 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #
 # Use this shell script to compile (if necessary) your code and then execute it. Belw is an example of what might be found in this file if your program was written in Python 3.7
-<<<<<<< HEAD
+
 python ./src/border_analytics_re_ed.py ./input/Border_Crossing_Entry_Data.csv ./output/report.csv
-=======
-  python3.7 ./src/border_analytics.py ./input/Border_Crossing_Entry_Data.csv ./output/report.csv
->>>>>>> a96b81701c4a6433596ca612a7bfedc56337f3df
+
+
+
